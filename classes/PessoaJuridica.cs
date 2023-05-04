055c6a4a713c07134d300d5913bae7cdf6ae21b2
sovietkilla07 <sovietmoderator109@gmail.com>
2023-05-04 01:18:34 -0300

    pequena correção e criação na classe pessoa juridica

--- classes/PessoaJuridica.cs
+++ classes/PessoaJuridica.cs
@@ -5,10 +5,8 @@ using System.Threading.Tasks;
 
 namespace reforco_backend.classes
 {
-    public abstract class PessoaJuridica
+    public class PessoaJuridica : Pessoa
     {
-        public string? Nome {get; set;}
-
-        public float? Rendimento {get; set;}
+        public string? CNPJ {get; set;}
     }
 }
