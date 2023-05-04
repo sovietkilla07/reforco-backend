967ce0d8ea12c169bb3de6c89254f540b8a9035f
sovietkilla07 <sovietmoderator109@gmail.com>
2023-05-04 12:25:13 -0300

    classe pessoa juridica pronta

--- classes/PessoaJuridica.cs
+++ classes/PessoaJuridica.cs
@@ -8,5 +8,29 @@ namespace reforco_backend.classes
     public class PessoaJuridica : Pessoa
     {
         public string? CNPJ {get; set;}
+
+        public void Inserir(PessoaJuridica pj)
+        {
+            using (StreamWriter sw = new StreamWriter(pj.Nome + ".txt"))
+            {
+                sw.WriteLine($"{pj.Nome},{pj.Rendimento},{pj.CNPJ}");
+            }
+        }
+
+        public PessoaJuridica Ler(string nomePj)
+        {
+            PessoaJuridica pj = new PessoaJuridica();
+
+            using (StreamReader sr = new StreamReader(nomePj + ".txt"))
+            {
+                string[] atributos = sr.ReadLine()!.Split(',');
+
+                pj.Nome = atributos[0];
+                pj.Rendimento = float.Parse(atributos[1]);
+                pj.CNPJ = atributos[2];
+            }
+
+            return pj;
+        }
     }
 }
